79ec82452efafb5d20fd448fd30df8f4a855f9db
grifonna8 <grifonna8@gmail.com>
2020-01-24 15:13:27 +0300

    additional homework for 2nd lesson

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-let money = 50000,
-income = '10000',
-addExpenses = 'Еда, съём, развлечения, интернет, животные',
-deposit = true,
-mission = 1000000,
-period = 8,
-budgetDay = null;
+let num = 266219,
+numArr = [],
+multiply = 1,
+exponentiation = 1,
+multiplyTripple = 0;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', mission, 'рублей');
-console.log((addExpenses.toLowerCase()).split(', '));
-budgetDay = money / 30;
-console.log(budgetDay);
-// alert("Она пытается в JS");
+numArr = String(num).split('');
+for (let i = 0; i < numArr.length; i++){
+  numArr[i] = Number.parseInt(numArr[i]);
+}
+for (let i = 0; i < numArr.length; i++){
+  multiply *= numArr[i]; 
+}
+console.log(multiply);
+multiplyTripple = multiply ** 3;
+console.log(multiplyTripple);
+
+numArr = String(multiplyTripple).split('');
+for (let i = 0; i < 2; i++){
+  numArr[i] = Number.parseInt(numArr[i]);
+  console.log(numArr[i]);
+}
