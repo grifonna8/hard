dabea30bebd106a6e3f028500a7fcdb5e29c3fca
grifonna8 <grifonna8@gmail.com>
2020-01-25 15:23:29 +0300

    optimized code

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ exponentiation = 1,
 multiplyTripple = 0;
 
 numArr = String(num).split('');
+// for (let i = 0; i < numArr.length; i++){
+//   numArr[i] = Number.parseInt(numArr[i]);
+// }
 for (let i = 0; i < numArr.length; i++){
-  numArr[i] = Number.parseInt(numArr[i]);
-}
-for (let i = 0; i < numArr.length; i++){
-  multiply *= numArr[i]; 
+  multiply *= +numArr[i]; /* добавила + для явного преобразования в число */
 }
 console.log(multiply);
 multiplyTripple = multiply ** 3;
@@ -17,6 +17,6 @@ console.log(multiplyTripple);
 
 numArr = String(multiplyTripple).split('');
 for (let i = 0; i < 2; i++){
-  numArr[i] = Number.parseInt(numArr[i]);
-  console.log(numArr[i]);
+  // numArr[i] = Number.parseInt(numArr[i]); сократили запись за счет использования + ниже
+  console.log(+numArr[i]);
 }
