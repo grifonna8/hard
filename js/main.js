7dc50797f16d31db66905ffccb7b592a804f6ff4
grifonna8 <grifonna8@gmail.com>
2020-01-29 16:09:15 +0300

    hard lesson 04

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-let money = 50000,
-income = '10000',
-addExpenses = 'Еда, съём, развлечения, интернет, животные',
-deposit = true,
-mission = 1000000,
-period = 8,
-budgetDay = null;
+'use strict';
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', mission, 'рублей');
-console.log((addExpenses.toLowerCase()).split(', '));
-budgetDay = money / 30;
-console.log(budgetDay);
-// alert("Она пытается в JS");
+function change(data){
+  if (typeof(data) !== 'string'){
+    console.log('введена не строка');
+  } else if (typeof(data) === 'string' && data.length < 30){
+    console.log(data.trim());
+  } else if(typeof(data) === 'string' && data.length >= 30){
+    console.log(data.substring(0,30) + '...');
+  }
+}
+let variable = change('123456789123456789123456789123456789');
