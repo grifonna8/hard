f89fe8fbfc6b9992968219e41ebbfc2f52fc049e
grifonna8 <grifonna8@gmail.com>
2020-01-30 13:12:18 +0300

    did 2 ways of calling the function, both working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 'use strict';
 
+// function change(data){
+//   if (typeof(data) !== 'string'){
+//     console.log('введена не строка');
+//   } else {
+//     if ((data.trim()).length < 30){
+//       console.log(data.trim());
+//     } else {
+//       let m = '';
+//       m = data.trim();
+//       console.log(m.substring(0,30) + '...');
+//     }
+//   }
+// }
+// let variable = change('     123456789123456789123456789123456789');
+
 function change(data){
   if (typeof(data) !== 'string'){
-    console.log('введена не строка');
-  } else if (typeof(data) === 'string' && data.length < 30){
-    console.log(data.trim());
-  } else if(typeof(data) === 'string' && data.length >= 30){
-    console.log(data.substring(0,30) + '...');
+    return 'введена не строка';
+  } else {
+    if ((data.trim()).length < 30){
+      return data.trim();
+    } else {
+      let m = '';
+      m = data.trim();
+      return m.substring(0,30) + '...';
+    }
   }
 }
-let variable = change('123456789123456789123456789123456789');
+console.log(change('     123456789123456789123456789123456789'));
