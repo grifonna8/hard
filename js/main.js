4eb090ff859d5d5509252fdf29b13255fa63e9d3
grifonna8 <grifonna8@gmail.com>
2020-02-03 21:22:09 +0300

    hard homework for lesson 6

--- js/main.js
+++ js/main.js
@@ -1,18 +1,67 @@
-let money = 50000,
-income = '10000',
-addExpenses = 'Еда, съём, развлечения, интернет, животные',
-deposit = true,
-mission = 1000000,
-period = 8,
-budgetDay = null;
+'use strict';
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', mission, 'рублей');
-console.log((addExpenses.toLowerCase()).split(', '));
-budgetDay = money / 30;
-console.log(budgetDay);
-// alert("Она пытается в JS");
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+let isNumber = function(n){
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+let answer = +prompt('Угадай число от 1 до 100');
+function bot(){
+  const a = randomInteger(1, 100);
+  let m = 10;
+  function bot2(){
+    if (isNumber(answer) && answer !== 0){
+      if (answer > a){
+        m--;
+        answer = +prompt('Загаданное число меньше, осталось попыток ' + m);
+        if (m === 1) {
+          answer = confirm('Попытки закончились, хотите сыграть еще?');
+          if (answer === true) {
+            answer = +prompt('Угадай число от 1 до 100');
+            m = 10;
+            return bot2();
+          } else {
+            return alert('Игра окончена');
+          }
+        }
+        return bot2();
+      } else if (answer < a && answer !== 0){
+        m--;
+        answer = +prompt('Загаданное число больше, осталось попыток ' + m);
+        if (m === 1) {
+          answer = confirm('Попытки закончились, хотите сыграть еще?');
+          if (answer === true) {
+            answer = +prompt('Угадай число от 1 до 100');
+            m = 10;
+            return bot2();
+          } else {
+            return alert('Игра окончена');
+          }
+        }
+        return bot2();
+      } else if (answer === a && answer !== 0){
+        answer = confirm('Поздравляю, Вы угадали! Хотели бы сыграть еще?');
+        if (answer === true) {
+          answer = +prompt('Угадай число от 1 до 100');
+          return bot2();
+        } else {
+          return alert('Игра окончена');
+        }
+      } else if (answer === 0) {
+        return alert('Игра окончена');
+      }
+    } else if (answer !== 0){
+      alert('Введи число');
+      answer = +prompt('Угадай число от 1 до 100');
+      return bot(); 
+    } else if (answer === 0){
+      return alert('Игра окончена');
+    }
+  }  
+  bot2();
+}
+
+bot();
+
